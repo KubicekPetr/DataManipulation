3c5b1c682aab32ef51a227cd29c32a4bbf02d8e1
KubicekPetr <kubicekpetr@outlook.com>
2020-12-29 03:20:33 +0100

    init of manipulable charts in preact

---
+++ manipulable-charts/src/index.js
@@ -0,0 +1,51 @@
+import './style';
+import { Component, render } from 'preact';
+
+function getTicks(count, max) {
+	// destructuring not working now 
+	return Array.from(Array(count).keys()).map(d => max / (count - 1) * parseInt(d));
+}
+
+
+class LineChart extends Component {
+	render ({ data }) {
+    	let WIDTH = 500;
+        let HEIGHT = 300;
+        let TICK_COUNT = 5;
+        let MAX_X = Math.max(...data.map(d => d.x));
+        let MAX_Y = Math.max(...data.map(d => d.y));
+        
+        let x = val => val / MAX_X * WIDTH;
+        let y = val => HEIGHT - val / MAX_Y * HEIGHT;
+        let x_ticks = getTicks(TICK_COUNT, MAX_X);
+        let y_ticks = getTicks(TICK_COUNT, MAX_Y).reverse();
+                
+        let d = `M${x(data[0].x)} ${y(data[0].y)} ${data.slice(1).map(d => {
+        	return `L${x(d.x)} ${y(d.y)}`;
+        }).join(' ')}`;
+    
+  	    return (
+        	<div class="LineChart" style={`width: ${WIDTH}px; height: ${HEIGHT}px`}>
+                <svg width={WIDTH} height={HEIGHT}>
+                    <path d={d} />
+                </svg>
+                <div class="x-axis">
+                    {x_ticks.map(v => <div data-value={v} />)}
+        	    </div>
+                <div class="y-axis">
+                    {y_ticks.map(v => <div data-value={v} />)}
+                </div>
+        	</div>
+        );
+    }
+}
+
+let data = [
+	{x: 0, y: 10}, 
+    {x: 10, y: 40}, 
+    {x: 20, y: 30}, 
+    {x: 30, y: 70}, 
+    {x: 40, y: 0}
+];
+
+render(<LineChart data={data} />, document.body);
