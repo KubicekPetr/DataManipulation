efe4c195164b7e1e2fbe6f69e29d102080ea88a5
KubicekPetr <kubicekpetr@outlook.com>
2020-12-30 22:01:27 +0100

    [WIP] working on algorithm

--- manipulable-charts/src/components/TerciaryInput.js
+++ manipulable-charts/src/components/TerciaryInput.js
@@ -4,18 +4,44 @@ class TerciaryInput extends Component {
 
     constructor() {
         super();
-        this.state = { terciaryInput: "" };
+        this.state = { 
+            terciaryInput: "",
+            sequence: "abcdefghijklmnopqrstuvwxyz0123456789",
+        };
         this.onHandleStep = this.onHandleStep.bind(this);
     }
 
     onHandleStep = e => {
-        let value = e.target.value;
-        this.setState({ [e.target.name]: value });
+        const value = e.target.value;
+        const newLetter = value[value.length - 1];
+        let sequence = this.state.sequence;
+        if (newLetter === "i") {
+            sequence = sequence.slice(0, sequence.length / 3);
+            console.log(sequence)
+        } else if (newLetter === "o") {
+            sequence = sequence.slice(sequence.length / 3, 2 * sequence.length / 3);
+            console.log(sequence)
+        } else if (newLetter === "p") {
+            sequence = sequence.slice(2 * sequence.length / 3);
+            console.log(sequence)
+        }
+        this.setState({sequence});
+        if (sequence.length <= 1) {
+            this.setState(prev => ({ 
+                [e.target.name]: prev[e.target.name] + sequence,
+                sequence: "abcdefghijklmnopqrstuvwxyz0123456789",
+            }));
+        } else {
+            this.setState(prev => ({ 
+                [e.target.name]: prev[e.target.name] + "",
+            }));
+        }
     }
 
     render() {
         return(
             <>
+                <div>{this.state.sequence}</div>
                 <input
                     type="text"
                     name="terciaryInput"
