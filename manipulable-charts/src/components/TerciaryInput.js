a7c168d01fe5d47f7a505d2c60c529fed47a1007
KubicekPetr <kubicekpetr@outlook.com>
2021-01-01 22:20:51 +0100

    code splitting

--- manipulable-charts/src/components/TerciaryInput.js
+++ manipulable-charts/src/components/TerciaryInput.js
@@ -1,4 +1,4 @@
-import { Component, render } from 'preact';
+import { Component } from 'preact';
 
 class TerciaryInput extends Component {
 
@@ -8,7 +8,7 @@ class TerciaryInput extends Component {
             terciaryInput: "",
             sequence: "abcdefghijklmnopqrstuvwxyz0123456789",
         };
-        this.onHandleStep = this.onHandleStep.bind(this);
+        this.onUserIsTyping = this.onUserIsTyping.bind(this);
     }
 
     onKeyDown = e => {
@@ -19,21 +19,22 @@ class TerciaryInput extends Component {
         }
     }
 
-    onHandleStep = e => {
+    handleStep = (e) => {
         const value = e.target.value;
         const newLetter = value[value.length - 1];
         let sequence = this.state.sequence;
         if (newLetter === "i") {
             sequence = sequence.slice(0, sequence.length / 3);
-            console.log(sequence)
         } else if (newLetter === "o") {
             sequence = sequence.slice(sequence.length / 3, 2 * sequence.length / 3);
-            console.log(sequence)
         } else if (newLetter === "p") {
             sequence = sequence.slice(2 * sequence.length / 3);
-            console.log(sequence)
         }
         this.setState({sequence});
+    }
+
+    writeCharacterAndResetSequence = (e) => {
+        const { sequence } = this.state;
         if (sequence.length <= 1) {
             this.setState(prev => ({ 
                 [e.target.name]: prev[e.target.name] + sequence,
@@ -46,6 +47,11 @@ class TerciaryInput extends Component {
         }
     }
 
+    onUserIsTyping = e => {
+        this.handleStep(e);
+        this.writeCharacterAndResetSequence(e);
+    }
+
     render() {
         return(
             <>
@@ -54,7 +60,7 @@ class TerciaryInput extends Component {
                     type="text"
                     name="terciaryInput"
                     value={this.state.terciaryInput}
-                    onInput={this.onHandleStep}
+                    onInput={this.onUserIsTyping}
                     onKeyDown={this.onKeyDown}
                 />
             </>
