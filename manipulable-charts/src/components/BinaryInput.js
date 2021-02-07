607854a9bc00b43ba59e9a2b458a3b47a63efe25
KubicekPetr <kubicekpetr@outlook.com>
2021-02-07 08:28:01 +0100

    extracting function for dispatching characters and fixing undefined error while step is under 1

--- manipulable-charts/src/components/BinaryInput.js
+++ manipulable-charts/src/components/BinaryInput.js
@@ -30,29 +30,48 @@ class BinaryInput extends Component {
         }));
     }
 
+    checkAgainstSteps = (step) => {
+        if (step < 1) {
+            this.dispatchCharacter();
+            // clear array
+            this.setState({ pressed: [] });
+            return true;
+        }
+    }
+
+    dispatchCharacter = () => {
+        this.setState(({ binaryInput, position }) => ({
+            binaryInput: binaryInput + this.sequence[position],
+            // reset position
+            deleting: true,
+            position: this.sequence.length / 2,
+            step: this.sequence.length / 4,
+        }), () => this.handleLogging(this.sequence[this.state.position]));
+    }
+
     onKeyUp = () => {
         // when all pushed keys are released,
         // we can evaluate
         const { pressed } = this.state;
         if (pressed.includes(97) && pressed.includes(98) && !this.state.deleting) {
-            this.setState(({ binaryInput, position }) => ({
-                binaryInput: binaryInput + this.sequence[position],
-                // reset position
-                deleting: true,
-                position: this.sequence.length / 2,
-                step: this.sequence.length / 4,
-            }), () => this.handleLogging(this.sequence[this.state.position]));
+            this.dispatchCharacter();
         } else if (pressed.includes(97) && pressed.includes(98) && this.state.deleting) {
             this.setState(({ binaryInput }) => ({
                 binaryInput: binaryInput.slice(0, -1),
             }));
         } else if (pressed.includes(97)) {
+            if (this.checkAgainstSteps(this.state.step)) {
+                return;
+            }
             this.setState(({ position, step }) => ({
                 deleting: false,
                 position: position - step,
                 step: step / 2,
             }), () => this.handleLogging(this.sequence[this.state.position]));
         } else if (pressed.includes(98)) {
+            if (this.checkAgainstSteps(this.state.step)) {
+                return;
+            }
             this.setState(({ position, step }) => ({
                 deleting: false,
                 position: position + step,
